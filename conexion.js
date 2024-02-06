25040a9ed58e86019236d9f1748271b183ee6faf
tkl88 <90476895+tkl88@users.noreply.github.com>
2024-02-06 01:02:28 -0400

    Update conexion.js

--- conexion.js
+++ conexion.js
@@ -1,17 +1,31 @@
-import mysql from "mysql"
+import mysql from "mysql";
 
-
+// Crear la conexión a la base de datos
 export const db = mysql.createConnection({
     host: process.env.HOSTDB,
     port: process.env.PORTDB,
     user: process.env.USER, 
-    password:  process.env.PASSWORD,
+    password: process.env.PASSWORD,
     database: process.env.DATABASE
-})
+});
 
+console.log(  "host:", process.env.HOSTDB,
+    "port:", process.env.PORTDB,
+    "user: ",  process.env.USER, 
+    "password:", process.env.PASSWORD,
+    "database:", process.env.DATABASE)
 
-if(db){
-    console.log("Conexion creada")
-}else{
-    console.log("Sin conectar")
-}
+// Escuchar el evento de error
+db.connect(function(err) {
+    if (err) {
+        console.error('Error al conectar a la base de datos:', err.stack);
+        return;
+    }
+
+    console.log('Conexión establecida con el identificador', db.threadId);
+});
+
+// Manejar el evento de error
+db.on('error', function(err) {
+    console.error('Error en la conexión a la base de datos:', err);
+});
